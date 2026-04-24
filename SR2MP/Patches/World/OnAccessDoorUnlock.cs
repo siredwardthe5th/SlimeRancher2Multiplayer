0dfb1540ef6b28cd1e7acd7fc553967fbd713492
eddie <siredwardthe4th@gmail.com>
2026-04-24 19:02:06 -0400

    fixes pt2

--- SR2MP/Patches/World/OnAccessDoorUnlock.cs
+++ SR2MP/Patches/World/OnAccessDoorUnlock.cs
@@ -1,18 +1,20 @@
 ﻿using HarmonyLib;
 using Il2CppMonomiPark.SlimeRancher.UI.AccessDoor;
+using Il2CppMonomiPark.SlimeRancher.UI.Framework.Displays;
 using Il2CppMonomiPark.World;
 using SR2MP.Packets.World;
 
 namespace SR2MP.Patches.World;
 
-[HarmonyPatch(typeof(AccessDoorUIRoot), "UnlockDoor")]
+[HarmonyPatch(typeof(AccessDoorUIActivator), nameof(AccessDoorUIActivator.OnPurchaseMenuResult))]
 public static class OnAccessDoorUnlock
 {
-    public static void Postfix(AccessDoorUIRoot __instance)
+    public static void Postfix(AccessDoorUIActivator __instance, UIRuntimeDisplay display, bool result)
     {
+        if (!result) return;
         var packet = new AccessDoorPacket
         {
-            ID = __instance._door.Id,
+            ID = __instance._accessDoor.Id,
             State = AccessDoor.State.OPEN
         };
         Main.SendToAllOrServer(packet);
